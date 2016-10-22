7884c281b00d5639aceaaf1478610f0f00f605ea
Yunus Emre ÇETİN <me@yunusemrecetin.com.tr>
2016-10-22 16:15:18 -0700

    change gulp setting for os x and .html, .css, .js file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
-var gulp = require('gulp'),
+var os = require('os'),
+    gulp = require('gulp'),
     connect = require('gulp-connect'),
     open = require('gulp-open');
 
-gulp.task('connect', function() {
+var browser = os.platform() === 'linux' ? 'google-chrome' : (
+  os.platform() === 'darwin' ? 'google chrome' : (
+  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+
+gulp.task('serve', function() {
     connect.server({
         root: './',
         livereload: true
     });
     gulp.src('./').pipe(open({
         uri: 'http://localhost:8080/app/index.html',
-        app: 'chrome'
+        app: browser
     }));
-    gulp.watch(['./app/*.html'], ['html'], ['./*.css'], ['css']);
 });
 
 gulp.task('html', function () {
-    gulp.src('./app/*.html')
-        .pipe(connect.reload());
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./app/*.html'], ['html'], ['./*.css'], ['css']);
+  gulp.watch(['./app/*.html', './app/css/*.css', './app/js/*.js'], ['html']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('connect', ['serve', 'watch']);
